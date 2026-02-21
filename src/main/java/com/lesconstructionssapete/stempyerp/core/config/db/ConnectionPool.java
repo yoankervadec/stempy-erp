b42a15d30066d01f177b796d61141e7b416f662a
Yoan Kervadec <kervadecy@icloud.com>
2026-02-21 14:37:33 -0500

    refactor: adjust connection pool configuration for improved performance and resource management

--- src/main/java/com/lesconstructionssapete/stempyerp/core/config/db/ConnectionPool.java
+++ src/main/java/com/lesconstructionssapete/stempyerp/core/config/db/ConnectionPool.java
@@ -21,9 +21,19 @@ public class ConnectionPool {
       config.setJdbcUrl(dotenv.get("DB_URL"));
       config.setUsername(dotenv.get("DB_USER"));
       config.setPassword(dotenv.get("DB_PASSWORD"));
-      String poolSize = dotenv.get("DB_MAX_POOL_SIZE", "10");
+
+      String poolSize = dotenv.get("DB_MAX_POOL_SIZE", "6");
       config.setMaximumPoolSize(Integer.parseInt(poolSize));
+      config.setMinimumIdle(2);
+      config.setConnectionTimeout(30000); // 30 seconds
+      config.setIdleTimeout(600000); // 10 minutes
+      config.setMaxLifetime(1800000); // 30 minutes
+
       config.setDriverClassName("com.mysql.cj.jdbc.Driver");
+
+      config.addDataSourceProperty("cachePrepStmts", "true"); // enable prepared statement caching
+      config.addDataSourceProperty("prepStmtCacheSize", "100"); // number of prepared statements to cache
+      config.addDataSourceProperty("prepStmtCacheSqlLimit", "2048"); // character limit for cached SQL
 
       dataSource = new HikariDataSource(config);
 
